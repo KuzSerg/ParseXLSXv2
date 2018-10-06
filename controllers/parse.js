1dc9d2b1a0a9900e5a7bc7848cae3b63bba6259d
Sergey <43466490+KuzSerg@users.noreply.github.com>
2018-10-06 20:50:27 +0300

    Add Parse Upload XLSX

--- controllers/parse.js
+++ controllers/parse.js
@@ -1,16 +1,35 @@
 const xlsx2json = require('xlsx-2-json')
+const appRoot = require('app-root-path')
 
 module.exports.create = function (req, res) {
-// const body = req.body
-// const files =req.files
+    const filename = req.files[0].filename;
+    const path = req.files[0].destination;
+    const outpath = `${appRoot}\\outjson\\output.json`
+    const temp = `${appRoot}\\upload\\${filename}`
 
-
-    if((req.files) != null)
-    {
-        res.status(200).json(req.files)
-        console.log(req.files)
-        console.log(req.files[0].destination)
-    }else{
-        res.status(401).json({message:"Давай до свидания"})
-    }
+    xlsx2json({
+            input: `${temp}`,
+            output: `${outpath}`
+    }, 
+        function(err, result) {
+            if (err) {
+            return res.status(401).json(err);
+            } else {
+                result.forEach(result => {
+                    const obj = {
+                        Номер: result.Номер,
+                        Тип: result.Тип_БСО,
+                        Подразделение: result.Подразделение,
+                        Статус: result.Статус
+                    }
+                    console.log(obj)
+                    return obj
+                });
+                // console.log(result)
+                
+            }
+            return res.json(result)
+        }    
+    )
+         
 }
